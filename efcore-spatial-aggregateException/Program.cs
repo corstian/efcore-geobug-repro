54f6e65938c06c3c5330508841eec6a99ab6e70b
Corstian Boerman <mail@corstianboerman.com>
2018-10-25 16:16:12 +0200

    Add project files.

---
+++ efcore-spatial-aggregateException/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace efcore_spatial_aggregateException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
